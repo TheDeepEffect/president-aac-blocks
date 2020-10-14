d04bf60ac506a54f31cccc20a807e3937e3d2a26
thedeepeffect <deepmanek25@gmail.com>
2020-10-14 19:29:04 +0530

    removed navigate and icons smaller

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,20 +12,7 @@ const Footer = ({ logo }) => {
 					<p>Address</p>
 				</div>
 				<ul className="r-footer">
-					<li>
-						<h2>Navigate</h2>
-						<ul class="box">
-							<li>
-								<Link to="/">Home</Link>
-							</li>
-							<li>
-								<Link to="/">About Us</Link>
-							</li>
-							<li>
-								<Link to="/">Contact Us</Link>
-							</li>
-						</ul>
-					</li>
+					<li></li>
 					<li className="features">
 						<h2>Want to know more?</h2>
 						<ul className="box h-box">
@@ -48,16 +35,43 @@ const Footer = ({ logo }) => {
 						<h2>Follow Us</h2>
 						<ul className="box social-media">
 							<li>
-								<SocialIcon url="http://twitter.com/" fgColor="#fff" />
-								<SocialIcon url="http://instagram.com/" />
-								<SocialIcon url="http://facebook.com/" fgColor="#fff" />
-								<SocialIcon url="http://linkedin.com/" fgColor="#fff" />
+								<SocialIcon
+									url="http://twitter.com/"
+									fgColor="#fff"
+									style={{
+										height: "35px",
+										width: "35px",
+									}}
+								/>
+								<SocialIcon
+									url="http://instagram.com/"
+									style={{
+										height: "35px",
+										width: "35px",
+									}}
+								/>
+								<SocialIcon
+									url="http://facebook.com/"
+									fgColor="#fff"
+									style={{
+										height: "35px",
+										width: "35px",
+									}}
+								/>
+								<SocialIcon
+									url="http://linkedin.com/"
+									fgColor="#fff"
+									style={{
+										height: "35px",
+										width: "35px",
+									}}
+								/>
 							</li>
 						</ul>
 					</li>
 				</ul>
 				<div className="b-footer">
-					<p>All rights reserved &cpy;ABCD 2020</p>
+					<p>All rights reserved &copy;ABCD 2020</p>
 				</div>
 			</footer>
 		</div>
